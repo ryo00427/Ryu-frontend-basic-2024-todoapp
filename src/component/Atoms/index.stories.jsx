5328fcc83f2409845538f9199427c02b71b72f28
ryu sugiyama <2005ryu427@gmail.com>
2025-03-10 22:33:24 +0900

    Atoms-titleが完成しました

---
+++ src/component/Atoms/index.stories.jsx
@@ -0,0 +1,4 @@
+import { Title } from "C:/Users/sugi2/OneDrive/プログラミング/Ryu-frontend-basic-2024-todoapp/src/component/Atoms/Title/index.jsx";
+export default { component: Title };
+
+export const Default = {};
