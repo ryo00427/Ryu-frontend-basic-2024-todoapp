43c2346352aadb57d05f388f744821a192ffe8e7
ryu sugiyama <2005ryu427@gmail.com>
2025-03-12 23:32:22 +0900

    Inputが完成しました

---
+++ src/components/Atoms/Input/index.stories.jsx
@@ -0,0 +1,9 @@
+import { Input } from "./index";
+
+export default { component: Input };
+
+export const Default = {
+    args: {
+      onEditComplete: (value) => console.log("edit completed", value), 
+    },
+  };
