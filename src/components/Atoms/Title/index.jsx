b6bfee709b756c928686bccd4b33d7d3dbf07cc9
ryu sugiyama <2005ryu427@gmail.com>
2025-03-12 18:38:57 +0900

    パスを変更しました

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import FONTFAMILY from 'C:/Users/sugi2/OneDrive/プログラミング/Ryu-frontend-basic-2024-todoapp/src/variables/font_family.js';
-import COLOR from "C:/Users/sugi2/OneDrive/プログラミング/Ryu-frontend-basic-2024-todoapp/src/variables/color.js";
-import TEXTS from "C:/Users/sugi2/OneDrive/プログラミング/Ryu-frontend-basic-2024-todoapp/src/variables/texts.js";
+import FONTFAMILY from '../../../variables/font_family.js';
+import COLOR from "../../../variables/color";
+import TEXTS from "../../../variables/texts.js";
 
 export const Title = () => {
     return <StyledText>SlMPLE TODO APP</StyledText>
