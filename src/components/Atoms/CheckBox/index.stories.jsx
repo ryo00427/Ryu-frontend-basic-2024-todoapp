8b4b7ffb6792cba0f2d20d2d171d40962c90a9a5
ryu sugiyama <2005ryu427@gmail.com>
2025-03-12 23:05:35 +0900

    ckeckBoxが完成しました

---
+++ src/components/Atoms/CheckBox/index.stories.jsx
@@ -0,0 +1,10 @@
+import { Checkbox } from "./index";
+
+export default { component: Checkbox };
+
+export const Default = {
+    args: {
+        onClick: () => console.log("clicked"),
+      },
+    
+};
