80ac762fbe022cc82f62467b435963b300a7ce6d
ryu sugiyama <2005ryu427@gmail.com>
2025-03-12 18:00:17 +0900

    titleが完成しました

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import FONTFAMILY from '../../variables/font_family';
-import COLOR from "../../variables/color";
-import TEXTS from "/variables/texts";
+import FONTFAMILY from 'C:/Users/sugi2/OneDrive/プログラミング/Ryu-frontend-basic-2024-todoapp/src/variables/font_family.js';
+import COLOR from "C:/Users/sugi2/OneDrive/プログラミング/Ryu-frontend-basic-2024-todoapp/src/variables/color.js";
+import TEXTS from "C:/Users/sugi2/OneDrive/プログラミング/Ryu-frontend-basic-2024-todoapp/src/variables/texts.js";
 
 export const Title = () => {
-    return <StyledText>SIMPLE TODO APP</StyledText>
+    return <StyledText>SlMPLE TODO APP</StyledText>
 }
 
 const StyledText = styled.div`
-  font-family: ${FONTFAMILY.ROBOTO};
-  font-size: ${TEXTS.L};
-  color: ${COLOR.GREEN};
+   font-family: ${FONTFAMILY.ROBOTO};
+   ${TEXTS.L};
+  color: ${COLOR.WHITE};
 
   @media (max-width: 768px) {
-    font-size: ${TEXTS.M};  
+     ${TEXTS.M};  
   }
 `;
