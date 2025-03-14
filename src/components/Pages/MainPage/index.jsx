889601cd89ce12fd54a6437662786cd33d601d62
ryu sugiyama <2005ryu427@gmail.com>
2025-03-14 22:19:15 +0900

    MainPageができました

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -1,37 +1,36 @@
 import React from "react";
 import styled from "styled-components";
-import COLOR from "../../../variables/color";
 import { Title } from "../../Atoms/Title";
 import { TodoCard } from "../../Organisms/TodoCard";
 export const MainPage = ()=> {
   return (
     <StyledWrapper>
-     <Title/>
-     <TodoCard/>
-   </StyledWrapper>
+      <Title />
+      <StyledTaskList>
+        <TodoCard />
+      </StyledTaskList>
+    </StyledWrapper>
 );
   
 };
 const StyledWrapper = styled.div`
   display: flex;
-  padding: 20px;
-  overflow: hidden;
-  background-color: ${COLOR.BLACK};
-  border-radius: 2px;
-  gap: 0px;
-  border: none;
-  outline: none;
-  width: 100%;
   flex-direction: column;
-  margin-top: 10px;
+  align-items: center;
+  padding: 52px;
+  gap: 10px;
+  line-height: 52px;
+  @media (min-width: 768px) {
+        line-height: 52px;
+  }
 `;
 
 const StyledTaskList = styled.div`
-  background-color: ${COLOR.BLACK};
-  border-radius: 2px;
-  padding: 4 4px;
-  border: none;
-  outline: none;
-  width: 100%;
-  flex-direction: column
+  align-items: center;
+  width: 40%;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    align-items: center;
+    line-height: 52px;
+  }
 `;
