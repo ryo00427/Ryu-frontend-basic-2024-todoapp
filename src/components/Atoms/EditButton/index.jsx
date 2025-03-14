1a4ff30e381446a334edc0b5042d83708f098627
ryu sugiyama <2005ryu427@gmail.com>
2025-03-14 17:56:42 +0900

    cssを修正しました

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -23,7 +23,12 @@ const StyledButton = styled.button`
   border-radius: 50%;
   background-color: ${COLOR.LIGHT_GRAY_TRANSPARENT};
   transition: 0.2s;
-  
+
+  & > img {
+    width: 100%;
+    height: 100%;
+  }
+
   &:hover {
     background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
   }
