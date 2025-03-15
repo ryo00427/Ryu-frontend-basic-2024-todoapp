482899bbdca4ea17014b5e4dd65a9b8a072a3ec4
ryu sugiyama <2005ryu427@gmail.com>
2025-03-15 23:27:43 +0900

    修正しました

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import pencil from "../../../assets/svg/pencil.svg";
 import COLOR  from "../../../variables/color";
-export const EditButton = ({ onClick }) => {
+const EditButton = ({ onClick }) => {
   return (
     <StyledButton onClick={onClick}>
       <img src={pencil} />
     </StyledButton>
   );
 };
-
+export default EditButton;
 const StyledButton = styled.button`
   border: none;
   outline: none;
